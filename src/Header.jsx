1d9e5c7c33a3bd88bbcaceb58d43a87707563d2c
Manon <manonlefkos@gmail.com>
2023-10-12 23:39:09 +0200

    add custom TW color

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,9 @@ import { NavLinks } from "./NavLinks";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  console.log("rendering NavBar");
+  console.log("rendering Header");
   return (
-    <header className="flex flex-wrap justify-between p-5 h-16 bg-slate-400">
+    <header className="flex flex-wrap justify-between p-5 h-16  bg-green-test text-white">
       <NavLinks />
       <div className="flex gap-2">
         <Link to={`/cart`}>Cart</Link>
