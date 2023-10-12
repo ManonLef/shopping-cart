55fc31fea42a7bdd011e68d440253ab941d7d0db
Manon <manonlefkos@gmail.com>
2023-10-12 23:43:18 +0200

    add Fetch to router component

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,8 +3,13 @@ import App from "./App";
 import HomePage from "./Homepage";
 import Shop from "./Shop";
 import Cart from "./Cart";
+import Fetch from "./Fetch";
 
 const Router = () => {
+  const url =
+    "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}";
+  const { data, loading, error } = Fetch(url);
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -16,7 +21,7 @@ const Router = () => {
         },
         {
           path: "shop",
-          element: <Shop />,
+          element: <Shop data={data} loading={loading} error={error} />,
         },
         {
           path: "cart",
