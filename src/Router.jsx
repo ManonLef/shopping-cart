25c550b4c1f2d5da21f09e29026919f11d1d1490
Manon <manonlefkos@gmail.com>
2023-10-12 21:58:25 +0200

    add Router to its own component

---
+++ src/Router.jsx
@@ -0,0 +1,31 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import HomePage from "./Homepage";
+import Shop from "./Shop";
+import Cart from "./Cart";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/shop",
+          element: <Shop />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default Router
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./Homepage.jsx";
-import Shop from "./Shop.jsx";
-import Cart from "./Cart.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/shop",
-        element: <Shop />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>
 );
