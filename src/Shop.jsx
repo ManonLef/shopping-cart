0976c8624ab9e9cd29f3f28281b9aec897cc746e
Manon <manonlefkos@gmail.com>
2023-10-12 11:06:12 +0200

    add basic product component for each item in shop

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import Product from "./Product";
+
 export default function Shop() {
-  return <div className="flex-1 p-5">Shop content</div>;
+  // dummy products to display
+  const [products, setProducts] = useState([
+    { name: "prod1", price: "$5" },
+    { name: "prod2", price: "$10" },
+  ]);
+
+  return (
+    <div className="flex-1 p-5">
+      <div className="flex gap-3">
+      {products.map((each) => {
+        return <Product name={each.name} price={each.price} />;
+      })}
+      </div>
+    </div>
+  );
 }
