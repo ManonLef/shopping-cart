7e991382c1e72aff54a565b048652ce6cae60de6
Manon <manonlefkos@gmail.com>
2023-10-12 11:09:16 +0200

    formatting

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,10 @@
-const Product = ({name, price}) => {
+const Product = ({ name, price }) => {
   return (
     <div className="border rounded p-3 ">
       <div>{name}</div>
       <div>{price}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 import Product from "./Product";
 
 export default function Shop() {
+  console.log("shop.jsx rendering");
   // dummy products to display
   const [products, setProducts] = useState([
-    { name: "prod1", price: "$5" },
-    { name: "prod2", price: "$10" },
+    { name: "prod1", price: "$5", key: 1 },
+    { name: "prod2", price: "$10", key: 2 },
   ]);
 
   return (
     <div className="flex-1 p-5">
       <div className="flex gap-3">
-      {products.map((each) => {
-        return <Product name={each.name} price={each.price} />;
-      })}
+        {products.map((item) => {
+          return <Product key={item.key} name={item.name} price={item.price} />;
+        })}
       </div>
     </div>
   );
