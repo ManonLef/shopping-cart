e40039e21c561aa52b8e12ff4e0d199a564e0812
Manon <manonlefkos@gmail.com>
2023-10-13 12:05:37 +0200

    reorganise products data for the time being

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,12 +1,44 @@
-const Product = ({ ...props }) => {
-  console.log("rendering Product ", props.title);
+const Products = ({ data }) => {
+  //helper function to extract only the necessary data from props and store inside
+  console.log("prod", data);
   return (
-    <div className="border rounded p-3 w-2/12 bg-white">
-      <img src={props.image} alt="" />
-      <div>{props.title}</div>
-      <div>$ {props.price}</div>
+    <div className="flex-1 p-5">
+      <div className="flex gap-3 flex-wrap justify-center m-10">
+        {data.map((item) => {
+          return (
+            <Product
+              key={item.node.id}
+              title={item.node.title}
+              description={item.node.description}
+              image={item.node.featuredImage.url}
+              price={item.node.variants.edges[0].node.price.amount}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Product;
+const Product = ({ ...props }) => {
+  return (
+    <div className="flex-col border rounded p-3 w-2/12 bg-white">
+      <img src={props.image} alt="" />
+      <div className="flex whitespace-nowrap overflow-hidden">
+        {props.title}
+      </div>
+      <div>$ {props.price}</div>
+      <input
+        type="number"
+        name="amount"
+        id="amount"
+        min="0"
+        className="my-2 px-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="1"
+      />
+      <button className="bg-gray-100 px-2 w-full rounded">add to cart</button>
+    </div>
+  );
+};
+
+export default Products;
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,29 +1,19 @@
 import { useState } from "react";
-import Product from "./Product";
+import Products from "./Product";
+import Fetch from "./Fetch";
 
-export default function Shop({ data, error, loading }) {
+
+
+export default function Shop() {
+  const url =
+    "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}";
+
   console.log("rendering shop");
-  const [products, setProducts] = useState(data);
-  if (loading) return <div>Loading</div>;
-  if (error) return <div>error {error}</div>;
-  // const productsToEdit = data; // do actual product stuff here?
-  console.log("shop", products);
 
-  return (
-    <div className="flex-1 p-5">
-      <div className="flex gap-3 flex-wrap justify-center m-10">
-        {products.map((item) => {
-          return (
-            <Product
-              key={item.node.id}
-              title={item.node.title}
-              description={item.node.description}
-              image={item.node.featuredImage.url}
-              price={item.node.variants.edges[0].node.price.amount}   
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+  const { data, loading, error } = Fetch(url);
+
+  if (loading) return <div className="flex-1">Loading</div>;
+  if (error) return <div className="flex-1">error {error}</div>;
+
+  return <Products data={data}/>
 }
