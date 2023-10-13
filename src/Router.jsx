bb646d61909f5577888b6ff821e3f9c78c121d26
Manon <manonlefkos@gmail.com>
2023-10-13 12:04:32 +0200

    remove fetch from router

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,10 +6,6 @@ import Cart from "./Cart";
 import Fetch from "./Fetch";
 
 const Router = () => {
-  const url =
-    "https://mock.shop/api?query={products(first:%2020){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}";
-  const { data, loading, error } = Fetch(url);
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -21,7 +17,7 @@ const Router = () => {
         },
         {
           path: "shop",
-          element: <Shop data={data} loading={loading} error={error} />,
+          element: <Shop />,
         },
         {
           path: "cart",
