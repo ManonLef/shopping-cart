254cd362f440df03058451d78087ae0904796761
Manon <manonlefkos@gmail.com>
2023-10-13 09:57:23 +0200

    edit Header component to default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Footer from "./Footer";
-import { Header } from "./Header";
+import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log("rendering App")
+  console.log("rendering App");
   return (
     <div className="flex flex-col h-screen">
       <Header />
--- src/Header.jsx
+++ src/Header.jsx
@@ -17,4 +17,4 @@ const Header = () => {
   );
 };
 
-export { Header };
+export default Header;
